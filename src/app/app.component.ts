47e267e0f63a7287bad1a184c3e23fa8bd1ae385
Sudhir <sudhirmunna007@gmail.com>
2020-04-02 11:11:20 -0500

    react & vue app integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, AfterContentChecked } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'container-app';
   profile;
   data;
+  name;
+  reactOutput = 0;
+  vueMessage;
+  vueOutput;
+  @ViewChild('myDiv') divView: ElementRef;
+  @ViewChild('vueDiv') vueDiv: ElementRef;
+
   microAppAEvent(event) {
     if (event) {
       console.log('contaient app', event);
@@ -20,5 +27,31 @@ export class AppComponent {
     if (this.profile) {
       this.data = this.profile.detail;
     }
+    if (this.divView && this.divView.nativeElement && this.profile) {
+      this.divView.nativeElement.setAttribute('name', this.profile.detail.firstName);
+    }
+    if (this.vueDiv && this.vueDiv.nativeElement && this.profile) {
+      this.vueDiv.nativeElement.setAttribute('message', this.profile.detail.firstName + ' ' + this.profile.detail.lastName);
+    }
+  }
+  ngAfterViewInit() {
+    if (this.divView && this.divView.nativeElement) {
+      this.divView.nativeElement.setAttribute('onClickEvt', 'onClickEvt');
+      this.divView.nativeElement.addEventListener('onClickEvt', () => this.getDataFromReactApp());
+    }
+    if (this.vueDiv && this.vueDiv.nativeElement) {
+      this.vueDiv.nativeElement.setAttribute('vueEvent', 'vueEvent');
+      this.vueDiv.nativeElement.addEventListener('vueEvent', (event) => this.getDataFromVueApp(event));
+    }
+  }
+
+  getDataFromReactApp() {
+    this.reactOutput = this.reactOutput + 1;
+  }
+  getDataFromVueApp(event: any ) {
+    console.log('event');
+    console.log(event);
+    this.vueOutput = event.detail;
+
   }
 }
